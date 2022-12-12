e1397e8f4bddf4d845a690aaf05d0eef7ccb8395
Sajahh <mariavaicomoutras@hotmail.com>
2022-12-12 15:49:43 -0300

    ajeitando app server

--- app.py
+++ app.py
@@ -9,49 +9,48 @@ from Gerar_Contas_Pagar_Receber_Seguro  import upload_seguro
 from Gerar_Contas_Pagar_Receber_Flash  import upload_flash
 from Criar_Invoice_Wide_Brazil  import download_invoice
 
-def app_backend():
-
-    app = Flask(__name__)
-    CORS(app)
-
-    def _build_cors_preflight_response():
-        response = make_response()
-        response.headers.add("Access-Control-Allow-Origin", "*")
-        response.headers.add("Access-Control-Allow-Headers", "*")
-        response.headers.add("Access-Control-Allow-Methods", "*")
-        return response
 
 
-    @app.route('/upload-decimo')
-    def uploadDecimo():
-        _build_cors_preflight_response()
-        upload_decimo_terceiro()
-        return "Success", 200
+app = Flask(__name__)
+CORS(app)
 
-    @app.route('/upload-ferias')
-    def uploadFerias():
-        _build_cors_preflight_response()
-        upload_ferias()
-        return "Success", 200
+def _build_cors_preflight_response():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Headers", "*")
+    response.headers.add("Access-Control-Allow-Methods", "*")
+    return response
 
-    @app.route('/upload-seguro')
-    def uploadSeguro():
-        _build_cors_preflight_response()
-        upload_seguro()
-        return "Success", 200
 
-    @app.route('/upload-flash')
-    def uploadFlash():
-        _build_cors_preflight_response()
-        upload_flash()
-        return "Success", 200
+@app.route('/upload-decimo')
+def uploadDecimo():
+    _build_cors_preflight_response()
+    upload_decimo_terceiro()
+    return "Success", 200
 
-    @app.route('/download-invoice')
-    def downloadInvoice():
-        _build_cors_preflight_response()
-        download_invoice()
-        return "Success", 200
+@app.route('/upload-ferias')
+def uploadFerias():
+    _build_cors_preflight_response()
+    upload_ferias()
+    return "Success", 200
 
-    app.run(debug=True, port=8080)
+@app.route('/upload-seguro')
+def uploadSeguro():
+    _build_cors_preflight_response()
+    upload_seguro()
+    return "Success", 200
 
-    return app
+@app.route('/upload-flash')
+def uploadFlash():
+    _build_cors_preflight_response()
+    upload_flash()
+    return "Success", 200
+
+@app.route('/download-invoice')
+def downloadInvoice():
+    _build_cors_preflight_response()
+    download_invoice()
+    return "Success", 200
+
+app.run(debug=True, port=8080)
+
