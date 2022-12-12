10e1cc843fbbae9f5172604d0e031ab060b16f99
Sajahh <mariavaicomoutras@hotmail.com>
2022-12-12 11:37:24 -0300

    ajustando cors 3

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sys
-from flask import Flask
+from flask import Flask, make_response
 from flask_cors import CORS, cross_origin
 
 #sys.path.insert(0, r"C:\Users\lucia\OneDrive\Área de Trabalho\ProjetosDev\FlaskDemo\src\controladores")
@@ -10,36 +10,37 @@ from Gerar_Contas_Pagar_Receber_Flash  import upload_flash
 from Criar_Invoice_Wide_Brazil  import download_invoice
 
 app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+CORS(app)
+
+def _build_cors_preflight_response():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Headers", "*")
+    response.headers.add("Access-Control-Allow-Methods", "*")
+    return response
 
 
 @app.route('/upload-decimo')
-@cross_origin()
 def uploadDecimo():
     upload_decimo_terceiro()
     return "Décimo Funcionando"
 
 @app.route('/upload-ferias')
-@cross_origin()
 def uploadFerias():
     upload_ferias()
     return "Férias Funcionando"
 
 @app.route('/upload-seguro')
-@cross_origin()
 def uploadSeguro():
     upload_seguro()
     return "Seguro Funcionando"
 
 @app.route('/upload-flash')
-@cross_origin()
 def uploadFlash():
     upload_flash()
     return "Flash Funcionando"
 
 @app.route('/download-invoice')
-@cross_origin()
 def downloadInvoice():
     download_invoice()
     return "Invoice Gerado"
