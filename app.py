fee9c53d9b386b73cf5b21cdef80df762db9a7fd
Sajahh <mariavaicomoutras@hotmail.com>
2022-12-12 11:18:29 -0300

    ajustando cors 2

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import sys
 from flask import Flask
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 
 #sys.path.insert(0, r"C:\Users\lucia\OneDrive\Área de Trabalho\ProjetosDev\FlaskDemo\src\controladores")
 from Gerar_Contas_Pagar_Receber_Decimo_Terceiro import upload_decimo_terceiro
@@ -10,30 +10,36 @@ from Gerar_Contas_Pagar_Receber_Flash  import upload_flash
 from Criar_Invoice_Wide_Brazil  import download_invoice
 
 app = Flask(__name__)
-CORS(app)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/upload-decimo')
+@cross_origin()
 def uploadDecimo():
     upload_decimo_terceiro()
     return "Décimo Funcionando"
 
 @app.route('/upload-ferias')
+@cross_origin()
 def uploadFerias():
     upload_ferias()
     return "Férias Funcionando"
 
 @app.route('/upload-seguro')
+@cross_origin()
 def uploadSeguro():
     upload_seguro()
     return "Seguro Funcionando"
 
 @app.route('/upload-flash')
+@cross_origin()
 def uploadFlash():
     upload_flash()
     return "Flash Funcionando"
 
 @app.route('/download-invoice')
+@cross_origin()
 def downloadInvoice():
     download_invoice()
     return "Invoice Gerado"
