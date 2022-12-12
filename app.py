7c9922b2995307ede04dd4f16f6445387666c303
Sajahh <mariavaicomoutras@hotmail.com>
2022-12-12 10:18:06 -0300

    ajuste para deploy 1

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import sys
-import dotenv
 from flask import Flask
 
-sys.path.insert(0, r"C:\Users\lucia\OneDrive\Área de Trabalho\ProjetosDev\FlaskDemo\src\controladores")
+#sys.path.insert(0, r"C:\Users\lucia\OneDrive\Área de Trabalho\ProjetosDev\FlaskDemo\src\controladores")
 from Gerar_Contas_Pagar_Receber_Decimo_Terceiro import upload_decimo_terceiro
 from Gerar_Contas_Pagar_Receber_Ferias import upload_ferias
-from Gerar_Contas_Pagar_Receber_Seguro import upload_seguro
-from Gerar_Contas_Pagar_Receber_Flash import upload_flash
-from Criar_Invoice_Wide_Brazil import download_invoice
+from Gerar_Contas_Pagar_Receber_Seguro  import upload_seguro
+from Gerar_Contas_Pagar_Receber_Flash  import upload_flash
+from Criar_Invoice_Wide_Brazil  import download_invoice
 
 app = Flask(__name__)
 
